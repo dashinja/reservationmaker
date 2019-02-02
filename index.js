4dcc76138929bd9e9e1443eb564f4a4779659231
DaShinja <byron@dashinja.com>
2019-02-02 12:09:05 -0500

    understanding

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const mysql = require('mysql');
-require('dotenv').config();
+const express = require('express')
+const path = require('path')
+const mysql = require('mysql')
+require('dotenv').config()
 
 // 🔥 Use of eexpress
-const app = express();
+const app = express()
 // 💩 Our port 💁‍
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: process.env.SQL_USER,
   password: process.env.SQL_PW,
   database: 'reservation_db'
-});
+})
 
 connection.connect(err => {
-  if (err) throw err;
-  console.log('Connecting at id: ', connection.threadId);
-});
+  if (err) throw err
+  console.log('Connecting at id: ', connection.threadId)
+})
 
 let reserveData = [
   {
@@ -28,70 +28,73 @@ let reserveData = [
     phoneNumber: 4212746473,
     uniqueID: '12123'
   }
-];
+]
 
 // [📦] Here is the middleware of the Json parser
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Pages to serve
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 app.get('/reserve', (req, res) => {
-  res.sendFile(path.join(__dirname, '/reserve.html'));
-});
+  res.sendFile(path.join(__dirname, '/reserve.html'))
+})
 
 app.get('/tables', (req, res) => {
-  res.sendFile(path.join(__dirname, 'tables.html'));
-});
+  res.sendFile(path.join(__dirname, 'tables.html'))
+})
 
 // API service
 app.get('/api/reserve/', (req, res) => {
-  res.json(reserveData);
-});
+  res.json(reserveData)
+})
 
 app.get('/api/reserve/:id', (req, res) => {
   // we are getting the ID of the URL here
-  let uniqueId = req.params.id;
+  let uniqueId = req.params.id
 
   // we do a foor loop to view all the objcts inside our array of objects
   for (let i = 0; i < reserveData.length; i++) {
     // if obj name matches with ur UniqueID passed in the URL
-    if (uniqueId === reserveData[i].reserveID) {
+    if (uniqueId === reserveData[i].uniqueID) {
       // Render the JSON file
-      res.json(reserveData[i]);
+      res.json(reserveData[i])
     }
   }
-});
+})
 
 // Server post method
 app.post('/api/reserve', (req, res) => {
-  reserveData.push(req.body);
-  console.log(req.body);
+  reserveData.push(req.body)
+  console.log("I'm the req.body on server end:", req.body)
 
   let myQuery =
-    'INSERT INTO reservations SET res_name = ?, res_phone = ?, res_email = ?, res_unique_id = ?';
+    'INSERT INTO reservations SET res_name = ?, res_phone = ?, res_email = ?, res_unique_id = ?'
   let options = [
     req.body.name,
     req.body.phoneNumber,
     req.body.email,
     req.body.uniqueID
-  ];
-  connection.query(myQuery, options, err => {
-    if (err) throw err;
-    console.log('Reservation Data Saved to DB');
-  });
-
-  res.redirect('/');
-});
+  ]
+  connection
+    .query(myQuery, options, err => {
+      if (err) throw err
+      console.log('Reservation Data Saved to DB')
+    })
+    .then(stuff => {
+      console.log("I'm the newly pushed reserveData array", reserveData)
+      res.redirect(`/api/reserve/${req.body.uniqueID}`)
+    })
+})
 // reserve tab
 // table tab
 // [🔥] Here is working our app ⭐
 app.listen(PORT, function() {
-  console.log('App listening on PORT ' + PORT);
-});
+  console.log('App listening on PORT ' + PORT)
+})
 
 //  /* freddy is the coolest like evar
 //  byron is wearing a very nice shirt today
--- schema.sql
+++ schema.sql
@@ -1,16 +1,16 @@
-DROP DATABASE IF EXISTS reservation_db;
+-- DROP DATABASE IF EXISTS reservation_db;
 
-CREATE DATABASE reservation_db;
+-- CREATE DATABASE reservation_db;
 
 USE reservation_db;
 
-CREATE TABLE reservations (
-res_id INT NOT NULL AUTO_INCREMENT,
-res_name VARCHAR(50) NOT NULL,
-res_phone VARCHAR(10) NOT NULL,
-res_email VARCHAR(50) NOT NULL,
-res_unique_id INT(5) NOT NULL,
-PRIMARY KEY (res_id)
-);
+-- CREATE TABLE reservations (
+-- res_id INT NOT NULL AUTO_INCREMENT,
+-- res_name VARCHAR(50) NOT NULL,
+-- res_phone VARCHAR(10) NOT NULL,
+-- res_email VARCHAR(50) NOT NULL,
+-- res_unique_id INT(5) NOT NULL,
+-- PRIMARY KEY (res_id)
+-- );
 
 SELECT * FROM reservations;
